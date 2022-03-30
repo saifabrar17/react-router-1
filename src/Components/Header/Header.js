4da29b4643ef24d5ff9a91e752611eb252182a55
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-30 19:41:44 +0600

    added custom active link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
             <h2>Welcome To my fancy routeing website</h2>
             <nav>
-                <Link to="/home">HOME </Link>
-                <Link to="/friends">Friends</Link>
-                <Link to="/about">About</Link>
+                <CustomLink to="/home">HOME </CustomLink>
+                <CustomLink to="/friends">Friends</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
             </nav>
         </div>
     );
