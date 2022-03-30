1ed3c0410901f6a382018386a117c513fa141177
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-30 15:25:23 +0600

    routes er moddhe route, BrowserRouter use korlam

---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is about us.</h2>
+            <p>Wanna know about us?</p>
+            <p>Biriyani khawa</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <p>Hello Friends, How are you?</p>
+            <small>Parle kichu taka dhar dis</small>
+        </div>
+    );
+};
+
+export default Friends;<p>Hello Friends, How are you?</p>
